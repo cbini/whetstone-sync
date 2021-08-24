5605fbbbc8e9003cba390a574f92c71c28c5904f
Charlie Bini <cbini87@gmail.com>
2021-08-24 20:51:29 +0000

    shell

--- sync-users.sh
+++ sync-users.sh
@@ -1,2 +1,2 @@
-~/quotaguard/bin/qgtunnel python3.6 ~/datarobot/db_extracts/db_extracts.py --config ~/datarobot/db_extracts/config/whetstone.json
+~/datarobot/datagun/bin/qgtunnel ~/.virtualenvs/datagun/bin/python ~/datarobot/datagun/extract.py --config ~/datarobot/datagun/config/whetstone.json
 ~/.virtualenvs/whetstone/bin/python ~/datarobot/whetstone/sync-users.py
