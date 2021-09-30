930f9df530bb4ffa8b9e6138094807abddcb21b2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-09-30 18:27:50 -0500

    fix obsv grp update

--- sync-users.py
+++ sync-users.py
@@ -9,11 +9,8 @@ load_dotenv()
 
 WHETSTONE_CLIENT_ID = os.getenv("WHETSTONE_CLIENT_ID")
 WHETSTONE_CLIENT_SECRET = os.getenv("WHETSTONE_CLIENT_SECRET")
-WHETSTONE_USERNAME = os.getenv("WHETSTONE_USERNAME")
-WHETSTONE_PASSWORD = os.getenv("WHETSTONE_PASSWORD")
 WHETSTONE_DISTRICT_ID = os.getenv("WHETSTONE_DISTRICT_ID")
 WHETSTONE_USERS_IMPORT_FILE = os.getenv("WHETSTONE_USERS_IMPORT_FILE")
-WHETSTONE_OBSVGRPS_IMPORT_FILE = os.getenv("WHETSTONE_OBSVGRPS_IMPORT_FILE")
 
 WHETSTONE_CLIENT_CREDENTIALS = (WHETSTONE_CLIENT_ID, WHETSTONE_CLIENT_SECRET)
 
@@ -21,23 +18,16 @@ WHETSTONE_CLIENT_CREDENTIALS = (WHETSTONE_CLIENT_ID, WHETSTONE_CLIENT_SECRET)
 def main():
     ws = whetstone.Whetstone()
     ws.authorize_client(client_credentials=WHETSTONE_CLIENT_CREDENTIALS)
-    ws.authorize_frontend(
-        district_id=WHETSTONE_DISTRICT_ID,
-        username=WHETSTONE_USERNAME,
-        password=WHETSTONE_PASSWORD,
-    )
 
     # load import users
     with open(WHETSTONE_USERS_IMPORT_FILE) as f:
         import_users = json.load(f)
 
-    new_users = []
+    print("Syncing users...")
     for u in import_users:
         # skip if inactive and already archived
         if u["inactive"] and u["inactive_ws"] and u["archived_at"]:
             continue
-
-        print(f"{u['user_name']} ({u['user_internal_id']})")
 
         # get IDs
         user_id = u["user_id"]
@@ -49,7 +39,7 @@ def main():
                 record_id=f"{user_id}/restore",
                 params={"district": WHETSTONE_DISTRICT_ID},
             )
-            print("\tReactivated")
+            print(f"\t{u['user_name']} ({u['user_internal_id']}) - REACTIVATED")
 
         # build user payload
         user_payload = {
@@ -74,9 +64,7 @@ def main():
 
                 user_id = create_resp.get("_id")
                 u["user_id"] = user_id
-
-                new_users.append(u)
-                print("\tCreated")
+                print(f"\t{u['user_name']} ({u['user_internal_id']}) - CREATED")
             else:
                 ws.put("users", user_id, body=user_payload)
         except Exception as xc:
@@ -87,35 +75,36 @@ def main():
         # archive
         if u["inactive"] and not u["archived_at"]:
             ws.delete("users", user_id)
-            print("\tArchived")
+            print(f"\t{u['user_name']} ({u['user_internal_id']}) - ARCHIVED")
             continue
 
-    print("Processing school observation group changes...")
-    with open(WHETSTONE_OBSVGRPS_IMPORT_FILE) as f:
-        import_obsvgrps = json.load(f)
-
-    for s in import_obsvgrps:
-        obsvgrps_payload = json.loads(s["observation_groups_dict"])
-        obsvgrps_payload["district"] = WHETSTONE_DISTRICT_ID
-        for nu in new_users:
-            if nu["school_id"] == s["school_id"]:
-                og_match = next(
-                    iter(
-                        [
-                            og
-                            for og in obsvgrps_payload["observationGroups"]
-                            if nu["group_name"] == og["name"]
-                        ]
-                    ),
-                    {},
-                )
-                role_user_ids = og_match.get(nu["group_type"])
-                role_user_ids.append(nu["user_id"])
-                print(
-                    f"\tAdded {nu['user_name']} to {nu['group_name']} as {nu['group_type']}"
-                )
-
-        ws.put("schools", record_id=s["school_id"], body=obsvgrps_payload)
+    print("\nProcessing school observation group changes...")
+    schools = ws.get("schools").get("data")
+    for s in schools:
+        print(f"\t{s['name']}")
+        obsv_grp_payload = {"district": WHETSTONE_DISTRICT_ID, "observationGroups": []}
+        school_users = [
+            u for u in import_users if u["school_id"] == s["_id"] and not u["inactive"]
+        ]
+        for grp in s.get("observationGroups"):
+            grp_change = False
+            grp_users = [su for su in school_users if su["group_name"] == grp["name"]]
+            grp_roles = {k: grp[k] for k in grp if k not in ["_id", "name"]}
+            grp_update = {"_id": grp["_id"], "name": grp["name"]}
+            for role, membership in grp_roles.items():
+                mem_ids = [m.get("_id") for m in membership]
+                role_users = [gu for gu in grp_users if gu["group_type"] == role]
+                for ru in role_users:
+                    if not ru["user_id"] in mem_ids:
+                        mem_ids.append(ru["user_id"])
+                        print(f"\t\tAdded {ru['user_name']} to {grp['name']}/{role}")
+                        grp_change = True
+                grp_update[role] = mem_ids
+            obsv_grp_payload["observationGroups"].append(grp_update)
+        if grp_change:
+            ws.put("schools", record_id=s["_id"], body=obsv_grp_payload)
+        else:
+            print("\t\tNo observation group changes")
 
 
 if __name__ == "__main__":
